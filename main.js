17a94425c05680bd5f7d21dd8f5b986b1d931ced
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-09 22:24:13 +0330

    fix the bug on javascript code

--- main.js
+++ main.js
@@ -5,22 +5,38 @@ const btnRight = document.querySelector(".button-right");
 const input = document.querySelector(".input-container");
 const errorMsg = document.querySelector(".error");
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-const email = 'example@example.com'
-btnRight.addEventListener("click", function () {
 
-   if(input.value.match(emailPattern)){
-    const newText = document.createElement("p");
+let isButtonIsClick = false;
+
+
+
+
+btnRight.addEventListener("click", function () {
+   if(!isButtonIsClick && input.value.match(emailPattern)){
+    isButtonIsClick = true;
+    
+    
+   const newText = document.createElement("p");
     newText.innerHTML = "Thank you for subscribing to our newsletter";
-    newText.classList.add("currect-message");
+    newText.classList.toggle("currect-message");
     input.insertAdjacentElement("afterend", newText);
     
-  } else if (!input.value.match(emailPattern)){
-    console.log('nist');
+     
+   } 
+   
+   else if (!isButtonIsClick && !input.value.match(emailPattern)) {
+    isButtonIsClick = true;
     input.style.border = "2px solid red";
     const createNewText = document.createElement("p");
     createNewText.innerHTML = "Please provide a valid email";
-    createNewText.classList.add("error-message");
+    createNewText.classList.toggle("error-message");
     input.insertAdjacentElement("afterend", createNewText);
     errorMsg.style.display = "block";
-  }
+   
+   }
+   
+
 });
+
+
+
