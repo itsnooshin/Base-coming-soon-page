0b06b6ccbd8098915a9e7465093f1cc3664d3a27
Nooshin bakhtiari <nooshindev@gmail.com>
2023-06-10 12:29:38 +0330

    add error and the right message

--- main.js
+++ main.js
@@ -4,39 +4,53 @@
 const btnRight = document.querySelector(".button-right");
 const input = document.querySelector(".input-container");
 const errorMsg = document.querySelector(".error");
+const validMsg = document.querySelector('.message')
 const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-let isButtonIsClick = false;
+let createNewText;
+btnRight.addEventListener("click", function (e) {
 
-
-
-
-btnRight.addEventListener("click", function () {
-   if(!isButtonIsClick && input.value.match(emailPattern)){
-    isButtonIsClick = true;
-    
-    
-   const newText = document.createElement("p");
-    newText.innerHTML = "Thank you for subscribing to our newsletter";
-    newText.classList.toggle("currect-message");
-    input.insertAdjacentElement("afterend", newText);
-    
-     
-   } 
-   
-   else if (!isButtonIsClick && !input.value.match(emailPattern)) {
-    isButtonIsClick = true;
-    input.style.border = "2px solid red";
-    const createNewText = document.createElement("p");
-    createNewText.innerHTML = "Please provide a valid email";
+  e.preventDefault();
+  const validFn = function () {
+    input.classList.toggle("input-valid");
+    input.classList.toggle("input-invalid");
+   createNewText = document.createElement("p");
+    createNewText.innerHTML =
+      "Thank you for your subscribing for our newsletter!";
     createNewText.classList.toggle("error-message");
+    createNewText.classList.toggle("currect-message");
+    input.insertAdjacentElement("afterend", createNewText);
+    errorMsg.style.display = "none";
+  };
+
+  const inValidFn = function () {
+    input.classList.toggle("input-invalid");
+    createNewText = document.createElement("p");
+    createNewText.innerHTML = "Please provide a valid email";
+    createNewText.classList.add("error-message");
     input.insertAdjacentElement("afterend", createNewText);
     errorMsg.style.display = "block";
-   
-   }
-   
+
+    
+  };
+
+  if (input.value.match(emailPattern)) {
+    validFn();
+    setTimeout(function () {
+      input.value = "";
+      input.style.border = "1px solid #ce9898";
+      createNewText.textContent = "";
+    }, 1200);
+  } 
+
+  if (!input.value.match(emailPattern)) {
+    inValidFn();
+    setTimeout(function(){
+      input.value = "";
+      input.style.border = "1px solid #ce9898";
+      createNewText.textContent = "";
+      errorMsg.style.display = "none";
+    },1200)
+  }
 
 });
-
-
-
